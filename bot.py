7a52a4a3bca2a4b7739afe077b7a8644ca7602df
Javier Rodríguez <103605265+xavitico1@users.noreply.github.com>
2025-05-15 14:12:33 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -82,4 +82,4 @@ def home():
 if __name__ == "__main__":
     from threading import Thread
     Thread(target=bot.polling).start()
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 8080)))
